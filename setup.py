e2610e73cb5693dc5f7c906b5ba7e26e87f1b615
Savannah Casto <89162068+SavannahCasto@users.noreply.github.com>
2022-05-27 14:53:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
-from my_pip_package import __version__
-
 setup(
     name='ga-test',
-    version='dev',
+    version='1.0.0',
 
     url='https://github.com/SavannahCasto/ga-test',
+    author='Author Name',
+    author_email='author@gmail.com',
+    packages=['ga-test'],
+
+    py_modules=['my_pip_package'],
 )
